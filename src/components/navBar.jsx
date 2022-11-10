18f59fe84d8d9ccf791501c0323f9bca7a8b762d
Bugs-art <111818703+Bugs-art@users.noreply.github.com>
2022-11-09 20:47:34 -0800

    Add files via upload

---
+++ src/components/navBar.jsx
@@ -0,0 +1,13 @@
+import './navBar.css';
+
+function NavBar(){
+    return (
+        <div className='navBar'>
+
+            <h1> Navigation items here </h1>
+            
+        </div>
+
+    )
+}
+export default NavBar;
